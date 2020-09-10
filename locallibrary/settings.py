a231c6630e72cec101319fe352692db36ab2aef1
Wen Jen <wjen1214@gmail.com>
2020-09-10 15:38:08 -0700

    change env var

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
