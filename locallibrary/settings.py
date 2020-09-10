729ee344068bfd0ed534daf1e2e2170556d9fd9c
Wen Jen <wjen1214@gmail.com>
2020-09-10 15:48:21 -0700

    change allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['www.locallibpy.herokuapp.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
