f990ef7f1a71cec04e02dd208d8f44254b9f3964
Wen Jen <wjen1214@gmail.com>
2020-09-10 15:42:12 -0700

    change debug

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 ALLOWED_HOSTS = ['www.locallibpy.herokuapp.com','127.0.0.1', 'localhost']
